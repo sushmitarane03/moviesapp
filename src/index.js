9d6c032cd9986b6300cf3006538e625eb0ab98f5
xghsxhgf <sushmitarane384@gmail.com>
2021-08-01 20:57:38 +0530

    Checkpoint 1

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    
+  </div>,
   document.getElementById('root')
 );
 
