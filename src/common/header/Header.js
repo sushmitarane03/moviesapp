e3078e55aae821d21971dab4df3d20eb399ddcb2
xghsxhgf <sushmitarane384@gmail.com>
2021-08-02 16:20:16 +0530

    Checkpoint 2

---
+++ src/common/header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+
+class Header extends React.Component {
+ render() {
+ return (
+ <div className="Header">
+ 
+    <img src='https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg' className="logo"/>
+    
+ </div>
+ )
+ }
+}
+export default Header; 
